ca3765d1469b1cf114e11f4bc25c92e1e806932a
LeronQ <qqiangllv@163.com>
2021-02-21 14:02:48 +0800

    二叉树前、中、后、层次递归及迭代遍历及深度

--- 精选 TOP 面试题/bianryTreeSearch.py
+++ 精选 TOP 面试题/bianryTreeSearch.py
@@ -1,42 +1,107 @@
 
 # 二叉树前、中、后遍历
 
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 前序遍历-递归
 class Solution:
-    #前序
-    def preorder(self,root,ans=[]):
-        if root!=None:
-            ans.append(root.val)
-        if root.left:
-            self.preorder(root.left,ans)
-        if root.right:
-            self.preorder(root.right,ans)
-        return ans
-    
-    #中序
-    def inorder(self,root,ans=[]):
-        # if root.left==None and root.right==None:
-        #     ans.append(root.val)
-        #     return
-        if root.left:
-            self.inorder(root.left,ans)
-        ans.append(root.val)
-        if root.right:
-            self.inorder(root.right,ans)
-        return ans
-    
-    #后序
-    def postorder(self,root,ans=[]):
-        if root.left:
-            self.postorder(root.left,ans)
-        if root.right:
-            self.postorder(root.right,ans)
-        ans.append(root.val)
-        return ans
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        def preorder(root):
+            nonlocal res
+            if not root:
+                return
+            res.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+        preorder(root)
+        return res
+
+# 前序遍历-迭代
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if not root:
+            return
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
+
+# 中序遍历-递归
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        def inorder(root):
+            nonlocal res
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return res
+
+# 中序遍历-迭代
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if not root:
+            return res
+        stack = []
+        cur = root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res
+
+
+# 后序遍历-递归
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        def postorder(root):
+            nonlocal res
+            if not root:
+                return
+            postorder(root.left)
+            postorder(root.right)
+            res.append(root.val)
+        postorder(root)
+        return res
+
+# 后序遍历-迭代
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if not root:
+            return res
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node.left :
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+            res.append(node.val)
+        return res[::-1]
 
 
 
