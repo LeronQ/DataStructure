9681c7c8d9f15224695edf4de060c939654f3963
LeronQ <qqiangllv@163.com>
2021-02-21 13:19:09 +0800

    二叉树前、中、后、层次遍历及深度

--- 精选 TOP 面试题/bianryTreeSearch.py
+++ 精选 TOP 面试题/bianryTreeSearch.py
@@ -69,3 +69,12 @@ class Solution:
         return res
 
 
+# 二叉树深度
+class Solution:
+    def TreeDepth(self, pRoot):
+        # write code here
+        if pRoot == None:
+            return 0
+        lDepth = self.TreeDepth(pRoot.left)
+        rDepth = self.TreeDepth(pRoot.right)
+        return max(lDepth,rDepth)+1
